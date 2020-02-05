cf4850552f0b82065f1f6bd928cc61445e69f93d
Alissar Arnaout <Alissar@Alissars-Air.fc.ca>
2020-02-05 09:03:01 -0500

    routing created

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// imports always go first at the top
+import splashPage from "./modules/splashPage.js";
+import appPage from "./modules/appPage.js";
+import errorPage from "./modules/errorPage.js";
+
+
+const router = new VueRouter({ // when we loud our site - we hit this first
+    routes: [
+        { path: '/', name: "splash", component: splashPage }, // will bring back this component  - will be rendered in html
+        { path: '/app', name: "app", component: appPage },
+        { path: '#', name: "error", component: errorPage}
+    ]
+})
+
+const vm = new Vue({
+    data: {
+
+    },
+
+    created: function() {
+        console.log('app is running');
+    },
+
+    methods: {
+
+    },
+
+    router // shorthand for router: router
+}).$mount("#app");
